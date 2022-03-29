d35cee580f0c6f2292b9d88cba650cf011476ce5
Eden Brekke <eden.lorrai@gmail.com>
2022-03-29 14:57:40 -0700

    acleaned up some code, added some errors, made our functions more uniform

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const Book = require('./models/book.js');
 
+
+
+mongoose.connect(process.env.DB_URL);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function () {
+  console.log('Mongoose is GOOOOO');
+})
+
 const app = express();
 app.use(cors());
 // add middleware to receive json as a request within express
@@ -13,44 +23,43 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3002;
 
-
-mongoose.connect(process.env.DB_URL);
-
-app.get('/', (request, response)=> {
+app.get('/', (request, response) => {
   response.send('well hiiii');
 })
 
 
-app.get('/books', async (request, response)=>{
-  const filteredQuery = {};
-
-  if(request.query.title){
-    filteredQuery.title = request.query.title
-  } 
-  const books = await Book.find(filteredQuery);
-
-  response.send(books)
-})
-
-app.post('/books', async (request, response)=> {
-  console.log(request.body);
-  try{
-    //Rest terms - post | Monogoose model.create
-    let createdBook = await Book.create(request.body);
-    response.status(201).send(createdBook);
-  } catch (error){
+app.get('/books', async (request, response, next) => {
+  try {
+    const filteredQuery = {};
+    if (request.query.title) {
+      filteredQuery.title = request.query.title
+    }
+    const books = await Book.find(filteredQuery);
+    response.status(200).send(books)
+  } catch (error) {
     next(error);
   }
 })
 
-app.delete('/books/:id', async (request, response)=> {
+app.post('/books', async (request, response) => {
+  console.log(request.body);
+  try {
+    //Rest terms - post | Monogoose model.create
+    let createdBook = await Book.create(request.body);
+    response.status(201).send(createdBook);
+  } catch (error) {
+    next(error);
+  }
+})
+
+app.delete('/books/:id', async (request, response) => {
   let id = request.params.id;
-  try{
+  try {
     // Rest term - delete | mongoose Model.findByIdAndDelete()
     console.log(id);
     await Book.findByIdAndDelete(id);
     response.status(204).send('Book Beleted');
-  } catch (error){
+  } catch (error) {
     next(error);
   }
 })
@@ -63,5 +72,10 @@ app.get('/test', (request, response) => {
 app.get('*', (request, response) => {
   response.status(404).send('No Such Directory');
 })
+
+//error
+app.use((error, request, response) => {
+  response.status(500).send(error.message);
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
