bd043e3b82be76e96ca5916a3dc58730abdeff79
Eden Brekke <eden.lorrai@gmail.com>
2022-03-29 14:05:22 -0700

    added post and delete functions and routes

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const Book = require('./models/book.js');
 
 const app = express();
 app.use(cors());
+// add middleware to receive json as a request within express
+app.use(express.json());
 
 const PORT = process.env.PORT || 3002;
 
@@ -17,6 +19,7 @@ mongoose.connect(process.env.DB_URL);
 app.get('/', (request, response)=> {
   response.send('well hiiii');
 })
+
 
 app.get('/books', async (request, response)=>{
   const filteredQuery = {};
@@ -29,6 +32,28 @@ app.get('/books', async (request, response)=>{
   response.send(books)
 })
 
+app.post('/books', async (request, response)=> {
+  console.log(request.body);
+  try{
+    //Rest terms - post | Monogoose model.create
+    let createdBook = await Book.create(request.body);
+    response.status(201).send(createdBook);
+  } catch (error){
+    next(error);
+  }
+})
+
+app.delete('/books/:id', async (request, response)=> {
+  let id = request.params.id;
+  try{
+    // Rest term - delete | mongoose Model.findByIdAndDelete()
+    console.log(id);
+    await Book.findByIdAndDelete(id);
+    response.status(204).send('Book Beleted');
+  } catch (error){
+    next(error);
+  }
+})
 app.get('/test', (request, response) => {
 
   response.send('test request received')
